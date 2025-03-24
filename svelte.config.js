b63ac4c6e28555f67f3a6337654081df7f569fe1
calebvetter <calebvetter@gmail.com>
2025-03-24 15:32:24 -0700

    Adjust build settings

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { Config } from '@sveltejs/kit';
 
-const config: Config = {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
-		appDir: 'app'
+		adapter: adapter()
 	}
 };
 
